0cd8fdca5ff6dd4584528425782994307d3c3d8d
Marlon <marlon.smikle@gmail.com>
2025-09-18 19:25:46 -0400

    Update WN10-AU-000005.ps1

--- STIGS/WN10-AU-000005.ps1
+++ STIGS/WN10-AU-000005.ps1
@@ -6,8 +6,8 @@
     Author          : Marlon Smikle
     LinkedIn        : linkedin.com/in/marlonsmikle/
     GitHub          : github.com/MarlonS9
-    Date Created    : 2024-09-18
-    Last Modified   : 2024-09-18
+    Date Created    : 2025-09-18
+    Last Modified   : 2025-09-18
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
